c32729588aaeee336655ff681446ff16441fa340
uzuntonev <georgi.uzuntonev@gmail.com>
2020-06-29 13:43:22 +0300

    return status code 400 if route is wrong

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,5 +4,5 @@ const auth = require('../middlewares/auth');
 module.exports = (app) => {
   app.use('/api/auth', router.auth);
   app.use('/api/products', auth(), router.product);
-  app.use('*', (req, res, next) => res.send('<h1> Wrong route maybe? </h1>'));
+  app.use('*', (req, res, next) => res.status(400).send('<h1> Wrong route maybe? </h1>'));
 };
