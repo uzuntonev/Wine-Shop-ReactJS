bb653dbee8910417af6f62f616bc7a5bb2016d77
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2020-08-14 17:19:31 +0300

    heroku deploy

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ dotenv.config();
 dbConnection()
   .then(() => {
     require('./config/express');
-    console.log('DB is connected...')
+    console.log('DB is connected...');
   })
   .catch((err) => console.error(err));
+
+const root = require('path').join(__dirname, '/client/build');
+app.use(express.static(root));
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root });
+});
